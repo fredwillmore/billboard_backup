0885018e0561762d3885e7562c2646d9daac1eaa
Fred Willmore <fredwillmore@gmail.com>
2023-12-02 08:52:15 -0800

    added visualization controller and tests

---
+++ app/controllers/visualizations_controller.rb
@@ -0,0 +1,19 @@
+class VisualizationsController < ApplicationController
+
+  def multi_line_chart
+    @year = visualizations_params[:year].to_i
+    @chart_length = visualizations_params[:chart_length]
+    respond_to do |format|
+      format.html
+      format.json do
+        if use_flat_file
+          render json: File.read("db/seeds/data/billboard/chart_tracks/#{@year}.json") rescue {}
+        else
+          start_date = Date.new(@year)
+          end_date = Date.new(@year+1)-1
+          render json: BillboardTrack.charting_tracks(start_date, end_date)
+        end
+      end
+    end
+  end
+end
