bbce6d70b9ac7f562ba5dee0f2855250eed9f225
Fred Willmore <fredwillmore@gmail.com>
2023-12-02 14:26:23 -0800

    getting MultiLineChart imported from old repo

---
+++ app/javascript/components/MultiLineChart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    this is MultiLineChart
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './app/javascript/components/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
