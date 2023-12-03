d7644ebd0500012f2cdd504c3e586c7bdd1e94c7
Fred Willmore <fredwillmore@gmail.com>
2023-12-02 16:53:14 -0800

    added babel.js to the toolchain and configured webpack to use it for loading jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,16 @@ module.exports = {
   entry: './app/javascript/components/index.jsx',
   resolve: {
     extensions: ['.js', '.jsx']
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
